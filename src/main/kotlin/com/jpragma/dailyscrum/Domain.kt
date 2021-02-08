cda4b846acd846987c915f101195a65c8cc73610
Isaac Levin <ilevin@jpragma.com>
2021-02-07 21:20:24 -0500

    removed all content from gh-pages branch

--- src/main/kotlin/com/jpragma/dailyscrum/Domain.kt
+++
@@ -1,5 +0,0 @@
-package com.jpragma.dailyscrum
-
-data class OrganizationUnit(var id: Int = -1, val name: String, val teams: List<Team> = emptyList())
-data class Team(var id:Int = -1, val name: String)
-data class Member(var id:Int = -1, val teamId: Int, val name: String, var title: String, var avatar: String? = null)
